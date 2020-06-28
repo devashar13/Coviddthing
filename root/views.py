e7ea141ab978ac34eb92de897384a5a31c5153ca
Dev Ashar <dev.ashar2019@vitstudent.ac.in>
2020-06-29 01:26:32 +0530

    Work

--- root/views.py
+++ root/views.py
@@ -7,11 +7,12 @@ def home_view(request):
     
     if request.method == 'POST':
         form = UserForm(request.POST,request.FILES) 
-        form.save() 
-        x_ray_img=request.FILES['x_ray_img']
-        result = image.GetResult(x_ray_img)
-        context={'result':result}
-        return render(request,'root/result.html',context)
+        if form.is_valid():
+            form.save() 
+            x_ray_img=request.FILES['x_ray_img']
+            result = image.GetResult(x_ray_img)
+            context={'result':result}
+            return render(request,'root/result.html',context)
         # if form.is_valid(): 
         #     form.save()
         #     x_ray_img=request.FILES['x_ray_img']
